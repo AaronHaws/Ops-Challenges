7e7110f8f1432f92c7de1293309c82da14b53e75
AaronHaws <aaronhaws@live.com>
2022-02-08 12:10:56 -0700

    removes nested git

---
+++ Ops-Challenges/Net-Tools/2/1/opschallenge09.py
@@ -0,0 +1,18 @@
+#! Python3
+
+def list():{
+    "1": 'Bird'
+    "2": 'Cat'
+    "3": 'Wolf'
+    "4": 'Bear'
+    "5": 'Fish'
+    "6": 'Worm'
+    "7": 'Dog'
+    "8": 'Rat'
+    "9": 'Cow'
+    "10": 'Monkey'
+}
+
+Print("4")
+
+
